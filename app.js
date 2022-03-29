cdaf6c7048aa799f1a4619d7eaee7e962f87c6a0
Ayush chauhan <aayushchauhanrajput47@gmail.com>
2022-03-29 14:37:24 +0530

    Committed

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const allSections = document.querySelector(".main-content");
 function PageTransitons() {
   // Button click active class
   for (let i = 0; i < sectBtn.length; i++) {
-    sectBtn[i];
+    sectBtn[i].addEventListener("click", function () {
+      let currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].className = currentBtn[0].className.replace(
+        "active-btn",
+        ""
+      );
+      this.className += "active-btn";
+    });
   }
 }
+
+PageTransitons();
