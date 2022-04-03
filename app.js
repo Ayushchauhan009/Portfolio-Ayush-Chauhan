9f8a0fa057f475b2ad1bb5ebb1893b9adc73cc2b
Ayush chauhan <aayushchauhanrajput47@gmail.com>
2022-04-03 11:58:34 +0530

    Header Section Ahead

--- app.js
+++ app.js
@@ -5,16 +5,33 @@ const allSections = document.querySelector(".main-content");
 
 function PageTransitons() {
   // Button click active class
-  for (let i = 0; i < sectBtn.length; i++) {
-    sectBtn[i].addEventListener("click", function () {
-      let currentBtn = document.querySelectorAll(".active-btn");
-      currentBtn[0].className = currentBtn[0].className.replace(
-        "active-btn",
-        ""
-      );
-      this.className += "active-btn";
+  sectBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const currBtn = e.target;
+      let currentBtn = document.querySelector(".active-btn");
+      currentBtn.classList.remove("active-btn");
+      currBtn.classList.add("active-btn");
     });
-  }
+  });
 }
+
+allSections.addEventListener("click", (e) => {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    sectBtns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+
+    // hide other buttons
+    sections.forEach((section) => {
+      section.classList.remove("active");
+    });
+
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
 
 PageTransitons();
